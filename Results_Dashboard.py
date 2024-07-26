bc09239ae04605189085f4ab47a3365025e813dd
joellaiwj <41282812+joellaiwj@users.noreply.github.com>
2024-07-26 21:03:51 +0800

    Update Results_Dashboard.py

--- Results_Dashboard.py
+++ Results_Dashboard.py
@@ -1323,9 +1323,9 @@ with tabs[3]:
 
                 if p_value <=0.05:
                     st.markdown(f"**:red[STEM Cluster {stem_cluster} vs SHAPE Cluster {shape_cluster}:]**")
-                    st.write(f"  - U statistic: {stat:.4f}")
-                    st.write(f"  - P-value: {p_value:.4f}")
-                    st.write(f"  - Effect size (r): {effect_size:.4f}")
+                    st.write(f"**  - U statistic: {stat:.4f}**")
+                    st.write(f"**  - P-value: {p_value:.4f}**")
+                    st.write(f"**  - Effect size (r): {effect_size:.4f}**")
                 else:
                     st.markdown(f"**STEM Cluster {stem_cluster} vs SHAPE Cluster {shape_cluster}:**")
                     st.write(f"  - U statistic: {stat:.4f}")
