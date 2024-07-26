72c50574da7604f19bb0ec217ca20617eef25e30
joellaiwj <41282812+joellaiwj@users.noreply.github.com>
2024-07-26 17:09:30 +0800

    Update Results_Dashboard.py

--- Results_Dashboard.py
+++ Results_Dashboard.py
@@ -1166,6 +1166,8 @@ with tabs[2]:
         
         st.dataframe(mapping_table,height=(len(column_label_mapping)+1)*35+3,use_container_width=True,hide_index=True)
 with tabs[3]:
+    st.dataframe(df_pre)
+    
     columns = ['2.1 (Q4_A_14)', '2.1 (Q4_A_13)', '2.1 (Q4_A_12)', '2.1 (Q4_A_11)',
         '2.1 (Q4_A_10)', '2.1 (Q4_A_9)', '2.1 (Q4_A_8)', '2.1 (Q4_A_7)',
         '2.1 (Q4_A_6)', '2.1 (Q4_A_5)', '2.1 (Q4_A_4)', '2.1 (Q4_A_3)',
