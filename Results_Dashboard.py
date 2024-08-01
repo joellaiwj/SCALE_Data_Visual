aa3bdcd7f76a37dbb5876c123eadd05510efb568
joellaiwj <41282812+joellaiwj@users.noreply.github.com>
2024-08-01 15:36:02 +0800

    Update Results_Dashboard.py

--- Results_Dashboard.py
+++ Results_Dashboard.py
@@ -18,7 +18,7 @@ from scipy.spatial.distance import cdist
 # Font size settings
 font_settings = dict(
     family="Arial, sans-serif",
-    size=14
+    size=12
 )
 
 # Calculate the total height based on the number of questions
