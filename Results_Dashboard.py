ab9cafcfc2a1220eb1747bd793e8bb90e119c708
joellaiwj <41282812+joellaiwj@users.noreply.github.com>
2024-08-08 16:17:05 +0800

    Update Results_Dashboard.py

--- Results_Dashboard.py
+++ Results_Dashboard.py
@@ -235,7 +235,7 @@ with tabs[1]:
         fig_diverging.add_annotation(
             x=50,  # X position of the annotation
             y=i+0.25,   # Y position of the annotation (index of the question)
-            text=Question_4_statements[i],  # The actual statement text
+            text=Question_4_statements[-i],  # The actual statement text
             showarrow=False,  # Hide the arrow
             xanchor='center',  # Anchor the text to the center
             yanchor='bottom',  # Anchor the text to the bottom
