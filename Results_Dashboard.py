2bd8ace2ff7800001bd108d05ddc425a70c2fe18
joellaiwj <41282812+joellaiwj@users.noreply.github.com>
2024-12-24 18:07:56 +0800

    Update Results_Dashboard.py

--- Results_Dashboard.py
+++ Results_Dashboard.py
@@ -260,7 +260,7 @@ with tabs[1]:
             y=-0.12,  # Position below the chart
             yanchor='top'
         ),
-        font=font_settings,
+        font=dict(color='black'),
         margin=dict(l=20, r=20, t=20, b=20),
         bargap=0.5
     )
